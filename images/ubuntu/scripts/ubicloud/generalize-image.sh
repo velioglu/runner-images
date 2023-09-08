4c044f1eba0cbd1f5941a6c791696d907981409f
Enes Cakir <enes@cakir.web.tr>
2023-09-08 16:29:10 +0300

    [ubicloud] Make Ubuntu x64 images Ubicloud compatible

---
+++ images/ubuntu/scripts/ubicloud/generalize-image.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -xe
+
+# To able run this image in Ubicloud, we need to remove some Azure specific
+# configurations
+
+sleep 30
+
+# It's Hyper-V Key Value Pair daemon, which is not needed in Ubicloud
+# It blocks booting the VM if it's not disabled
+systemctl disable hv-kvp-daemon.service
+
+# Delete the Azure Linux Agent
+apt -y purge walinuxagent
+rm -rf /var/lib/waagent
+rm -f /var/log/waagent.log
+
+# Clean up cloud-init logs and cache to run it again on first boot
+cloud-init clean --logs --seed
+
+# Delete Azure specific cloud-init config files
+rm -rf /etc/cloud/cloud.cfg.d/90-azure.cfg
+rm -rf /etc/cloud/cloud.cfg.d/10-azure-kvp.cfg
+
+# Replace cloud-init datasource_list with default list
+echo "# to update this file, run dpkg-reconfigure cloud-init
+datasource_list: [ NoCloud, ConfigDrive, OpenNebula, DigitalOcean, Azure, AltCloud, OVF, MAAS, GCE, OpenStack, CloudSigma, SmartOS, Bigstep, Scaleway, AliYun, Ec2, CloudStack, Hetzner, IBMCloud, Oracle, Exoscale, RbxCloud, UpCloud, VMware, Vultr, LXD, NWCS, None ]" > /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+
+# Delete Azure specific grub config files
+rm -rf /etc/default/grub.d/40-force-partuuid.cfg
+rm -rf /etc/default/grub.d/50-cloudimg-settings.cfg
+
+# Replace 50-cloudimg-settings with default grub settings
+echo "# Cloud Image specific Grub settings for Generic Cloud Images
+# CLOUD_IMG: This file was created/modified by the Cloud Image build process
+
+# Set the recordfail timeout
+GRUB_RECORDFAIL_TIMEOUT=0
+
+# Do not wait on grub prompt
+GRUB_TIMEOUT=0
+
+# Set the default commandline
+GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0\"
+
+# Set the grub console type
+GRUB_TERMINAL=console" >> /etc/default/grub.d/50-cloudimg-settings.cfg
+
+# Update grub
+update-grub
+
+# Remove all existing ssh host keys
+rm /etc/ssh/ssh_host_*key*
+
+# Delete the root password
+passwd -d root
+
+sync
+
+# Delete the packer account
+touch /var/run/utmp
+userdel -f -r packer
