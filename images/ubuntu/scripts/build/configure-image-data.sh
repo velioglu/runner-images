8a58605ef1107e84db2db373321a6b8d73c82f67
Enes Cakir <enes@cakir.web.tr>
2023-09-08 16:29:10 +0300

    [ubicloud] Make Ubuntu x64 images Ubicloud compatible

--- images/ubuntu/scripts/build/configure-image-data.sh
+++ images/ubuntu/scripts/build/configure-image-data.sh
@@ -13,10 +13,10 @@ os_version=$(lsb_release -rs)
 image_label="ubuntu-${os_version}"
 version_major=${os_version/.*/}
 version_wo_dot=${os_version/./}
-github_url="https://github.com/actions/runner-images/blob"
+github_url="https://github.com/ubicloud/runner-images/blob"
 
 software_url="${github_url}/ubuntu${version_major}/${image_version_major}.${image_version_minor}/images/ubuntu/Ubuntu${version_wo_dot}-Readme.md"
-releaseUrl="https://github.com/actions/runner-images/releases/tag/ubuntu${version_major}%2F${image_version_major}.${image_version_minor}"
+releaseUrl="https://github.com/ubicloud/runner-images/releases/tag/ubuntu${version_major}%2F${image_version_major}.${image_version_minor}"
 
 cat <<EOF > $imagedata_file
 [
