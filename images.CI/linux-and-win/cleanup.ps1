120630d9dcf525276dcf468143041ab571ffe171
Enes Cakir <enes@cakir.web.tr>
2023-10-31 22:34:31 +0300

    [ubicloud] Revert back to VHD image generation
    https://github.com/actions/runner-images/pull/8581

--- images.CI/linux-and-win/cleanup.ps1
+++ images.CI/linux-and-win/cleanup.ps1
@@ -1,12 +1,24 @@
 param(
-    [Parameter (Mandatory=$true)] [string] $TempResourceGroupName
+    [String] [Parameter (Mandatory=$true)] $TempResourceGroupName,
+    [String] [Parameter (Mandatory=$true)] $StorageAccount,
+    [String] [Parameter (Mandatory=$true)] $SubscriptionId,
+    [String] [Parameter (Mandatory=$true)] $ClientId,
+    [String] [Parameter (Mandatory=$true)] $ClientSecret,
+    [String] [Parameter (Mandatory=$true)] $TenantId
 )
 
-$groupExist = az group exists --name $TempResourceGroupName
+az login --service-principal --username $ClientId --password=$ClientSecret --tenant $TenantId | Out-Null
+az account set --subscription $SubscriptionId | Out-Null
+
+$groupExist = az group exists --name $TempResourceGroupName --subscription $SubscriptionId
 if ($groupExist -eq "true") {
+    $osDiskName = az deployment group list --resource-group $TempResourceGroupName --query "[].properties.parameters.osDiskName.value" -o tsv
     Write-Host "Found a match, deleting temporary files"
-    az group delete --name $TempResourceGroupName --yes | Out-Null
+    az group delete --name $TempResourceGroupName --subscription $SubscriptionId --yes | Out-Null
     Write-Host "Temporary group was deleted successfully"
+    Write-Host "Deleting OS disk"
+    az storage remove --account-name $StorageAccount -c "images" -n "$osDiskName.vhd" --only-show-errors | Out-Null
+    Write-Host "OS disk deleted"
 } else {
     Write-Host "No temporary groups found"
 }
