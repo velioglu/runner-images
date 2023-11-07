6e4274a61040ece47e0c38f2cab7c524eb80bef3
Enes Cakir <enes@cakir.web.tr>
2023-11-07 23:39:24 +0300

    [ubicloud] Add templates for Ubuntu arm64 images

--- images/ubuntu/scripts/build/install-kubernetes-tools.sh
+++ images/ubuntu/scripts/build/install-kubernetes-tools.sh
@@ -7,13 +7,14 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
 
 # Download KIND
-kind_url=$(resolve_github_release_asset_url "kubernetes-sigs/kind" "endswith(\"kind-linux-amd64\")" "latest")
+kind_url=$(resolve_github_release_asset_url "kubernetes-sigs/kind" "endswith(\"kind-linux-$(get_arch "amd64" "arm64")\")" "latest")
 kind_binary_path=$(download_with_retry "${kind_url}")
 
 # Supply chain security - KIND
-kind_external_hash=$(get_checksum_from_url "${kind_url}.sha256sum" "kind-linux-amd64" "SHA256")
+kind_external_hash=$(get_checksum_from_url "${kind_url}.sha256sum" "kind-linux-$(get_arch "amd64" "arm64")" "SHA256")
 use_checksum_comparison "${kind_binary_path}" "${kind_external_hash}"
 
 # Install KIND
@@ -32,14 +33,14 @@ curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 
 # Temporarily pinning the version
 # Download minikube
-curl -fsSL -O https://storage.googleapis.com/minikube/releases/v1.34.0/minikube-linux-amd64
+curl -fsSL -O https://storage.googleapis.com/minikube/releases/v1.34.0/minikube-linux-$(get_arch "amd64" "arm64")
 
 # Supply chain security - minikube
-minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-amd64" "1.34.0" "SHA256")
-use_checksum_comparison "minikube-linux-amd64" "${minikube_hash}"
+minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-$(get_arch "amd64" "arm64")" "1.34.0" "SHA256")
+use_checksum_comparison "minikube-linux-$(get_arch "amd64" "arm64")" "${minikube_hash}"
 
 # Install minikube
-install minikube-linux-amd64 /usr/local/bin/minikube
+install minikube-linux-$(get_arch "amd64" "arm64") /usr/local/bin/minikube
 
 # Install kustomize
 download_url="https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
--- images/ubuntu/scripts/build/install-ninja.sh
+++ images/ubuntu/scripts/build/install-ninja.sh
@@ -6,9 +6,10 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
 
 # Install ninja
-download_url=$(resolve_github_release_asset_url "ninja-build/ninja" "endswith(\"ninja-linux.zip\")" "latest")
+download_url=$(resolve_github_release_asset_url "ninja-build/ninja" "endswith(\"ninja-$(get_arch "linux" "linux-aarch64").zip\")" "latest")
 ninja_binary_path=$(download_with_retry "${download_url}")
 
 # Unzip the ninja binary
