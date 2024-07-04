df7a9b0c37b053632cf4e99bc37f42a3c9f2300d
Burak Velioglu <velioglub@ubicloud.com>
2024-07-04 19:02:38 +0300

    [ubicloud] Update OpenSSH version due to vulnerability
    Vulnerability link: https://ubuntu.com/security/CVE-2024-6387

--- images/ubuntu/scripts/ubicloud/install-packages.sh
+++ images/ubuntu/scripts/ubicloud/install-packages.sh
@@ -15,3 +15,12 @@ if is_ubuntu20; then
     systemctl start nftables
     systemctl enable nftables
 fi
+
+# Update OpenSSH (https://ubuntu.com/security/CVE-2024-6387)
+if is_ubuntu22; then
+    apt-get satisfy 'openssh-server (>= 1:8.9p1-3ubuntu0.10)'
+fi
+
+if is_ubuntu24; then
+    apt-get satisfy 'openssh-server (>= 1:9.6p1-3ubuntu13.3)'
+fi
