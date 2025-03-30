174b9da98e9ed33d42b2d2f3540aa9f564679526
Enes Cakir <enes@cakir.web.tr>
2025-03-30 08:55:53 +0300

    [ubicloud] Enable GCR docker mirrors
    Moving it from runtime to image generation to improve provisioning times
    
    https://github.com/ubicloud/ubicloud/pull/3078

--- images/ubuntu/scripts/ubicloud/configure-docker.sh
+++ images/ubuntu/scripts/ubicloud/configure-docker.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -xe
 
-DEFAULT_DOCKER_CONFIG='{
+DOCKER_DAEMON_CONFIG='{
   "experimental": false,
+  "registry-mirrors": ["https://mirror.gcr.io"],
   "dns-opts": [
     "attempts:3"
   ]
 }'
+
+BUILDKIT_CONFIG='
+[registry."docker.io"]
+  mirrors = ["mirror.gcr.io"]
+
+[registry."mirror.gcr.io"]
+  http = false
+  insecure = false'
+
 
 if [ -f "/etc/docker/daemon.json" ]; then
     existing_config=$(sudo cat /etc/docker/daemon.json)
 else
     existing_config="{}"
 fi
+echo "$existing_config" | jq ". += $DOCKER_DAEMON_CONFIG" | sudo tee /etc/docker/daemon.json
 
-echo "$existing_config" | jq ". += $DEFAULT_DOCKER_CONFIG" | sudo tee /etc/docker/daemon.json
+sudo mkdir -p /etc/buildkit
+echo "$BUILDKIT_CONFIG" | sudo tee -a /etc/buildkit/buildkitd.toml
 
+sudo systemctl daemon-reload
 sudo systemctl restart docker
